40bd48932530cec6cf9eff15791870cb50e634a0
Tymur_Mirzaiev <tymur.mirzaiev@wolterskluwer.com>
2023-07-28 13:42:01 +0300

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace SampleApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+
+        try
+        {
+            throw new Exception("Error in the application!");
+        }
+        catch(Exception ex)
+        {
+            _logger.LogError(ex, "Something went wrong! {Weather}", "pretty good!");
+        }
+
+        _logger.LogInformation($"Hello from {nameof(Get)} action!");
+
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
